4f31eff8be3f292dccc915d9263baa42e79d64da
christopherlam888 <christopher.peter.lam@gmail.com>
2021-09-18 10:13:14 -0400

    started working on list and cards for groceries

--- lib/groceries.dart
+++ lib/groceries.dart
@@ -10,16 +10,46 @@ class _GroceriesState extends State<Groceries> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.green,
-      body: Center(
-          child: Text(
-              "Groceries",
-              style: TextStyle(
-                fontSize: 40.0,
-                fontWeight: FontWeight.bold,
-                color: Colors.white,
-              )
+      body: Padding(
+        padding: const EdgeInsets.fromLTRB(10.0, 30.0, 10.0, 75.0),        child: Container(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Text(
+                "Day 1 - Day 7",
+                textAlign: TextAlign.center,
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              ),
+              Expanded(
+                child: ListView.builder(
+                  itemCount: 20,
+                  itemBuilder: (context, index){
+                    return Card(
+                      color: Colors.white,
+                      child: Text("Groceries"),
+                    );
+                  },
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton.extended(
+          onPressed: () {},
+          backgroundColor: Colors.pink,
+          icon: Icon(
+            Icons.clear,
+            color: Colors.black,
+          ),
+          label: Text(
+            "Reset All",
+            style: TextStyle(
+              color: Colors.black,
+            ),
           )
       ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
     );
   }
 }
