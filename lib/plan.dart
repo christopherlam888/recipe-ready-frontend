42c414a24da8cea767d4f391ba8926eb41f489a2
christopherlam888 <christopher.peter.lam@gmail.com>
2021-09-18 01:14:16 -0400

    started working on list, cards, and button for plan

--- lib/plan.dart
+++ lib/plan.dart
@@ -10,16 +10,43 @@ class _PlanState extends State<Plan> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.green,
-      body: Center(
-          child: Text(
-              "Plan",
-              style: TextStyle(
-                fontSize: 40.0,
-                fontWeight: FontWeight.bold,
-                color: Colors.white,
-              )
-          )
+      body: Padding(
+        padding: const EdgeInsets.fromLTRB(10.0, 10.0, 10.0, 75.0),
+        child: ListView.builder(
+          itemCount: 14,
+          itemBuilder: (context, index){
+            return Card(
+              color: Colors.white,
+              child: Center(
+                child: Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: Text(
+                    "Plan",
+                    style: TextStyle(
+                      fontSize: 20,
+                    ),
+                  ),
+                ),
+              ),
+            );
+          },
+        ),
       ),
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: () {},
+        backgroundColor: Colors.white,
+        icon: Icon(
+          Icons.add,
+          color: Colors.black,
+        ),
+        label: Text(
+          "Find Recipes",
+          style: TextStyle(
+            color: Colors.black,
+          ),
+        )
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
     );
   }
 }
