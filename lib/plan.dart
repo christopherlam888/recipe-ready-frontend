dbb3b7f9cd6fc2141f41591b4951219b9a597826
eggy <eggyrules@gmail.com>
2021-09-19 00:57:21 -0400

    plan: Add help text

--- lib/plan.dart
+++ lib/plan.dart
@@ -15,106 +15,166 @@ class Plan extends StatefulWidget {
 class _PlanState extends State<Plan> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Provider.of<StateTracker>(context, listen: true).darkMode
-          ? Colors.black87
-          : Colors.white,
-      body: Padding(
-        padding: const EdgeInsets.fromLTRB(10.0, 20.0, 10.0, 75.0),
-        child: Consumer<StateTracker>(builder: (context, state, child) {
-          return GroupedListView<dynamic, String>(
-            elements: state.recipes,
-            groupBy: (element) => DateFormat.yMMMd().format(element.date),
-            groupComparator: (value1, value2) => value2.compareTo(value1),
-            itemComparator: (item1, item2) => item1.name.compareTo(item2.name),
-            order: GroupedListOrder.DESC,
-            // useStickyGroupSeparators: true,
-            groupSeparatorBuilder: (String value) => Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Text(
-                value,
-                textAlign: TextAlign.left,
+    if (Provider.of<StateTracker>(context, listen: false).recipes.length == 0) {
+      return Scaffold(
+        backgroundColor:
+            Provider.of<StateTracker>(context, listen: false).darkMode
+                ? Colors.black
+                : Colors.white,
+        body: Padding(
+          padding: const EdgeInsets.all(40.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Text(
+                "Sorry, you do not have any saved recipes yet.",
+                textAlign: TextAlign.center,
                 style: TextStyle(
-                    fontSize: 20,
-                    fontWeight: FontWeight.bold,
-                    color: state.darkMode ? Colors.white : Colors.black),
-              ),
-            ),
-            itemBuilder: (c, element) {
-              return Slidable(
-                actionPane: SlidableDrawerActionPane(),
-                actions: <Widget>[
-                  IconSlideAction(
-                    caption: "Favourite",
-                    color: Colors.yellow[600],
-                    icon: Icons.star_border_sharp,
-                    onTap: () {},
-                  ),
-                ],
-                secondaryActions: <Widget>[
-                  IconSlideAction(
-                    caption: "Delete",
-                    color: Colors.red,
-                    icon: Icons.delete,
-                    onTap: () {},
-                  ),
-                ],
-                child: Card(
-                  elevation: 8.0,
-                  margin:
-                      new EdgeInsets.symmetric(horizontal: 10.0, vertical: 4.0),
-                  child: Container(
-                    decoration: BoxDecoration(
-                      image: DecorationImage(
-                        colorFilter: new ColorFilter.mode(
-                            Colors.black.withOpacity(0.5), BlendMode.darken),
-                        image: NetworkImage(element.imageLink),
-                        fit: BoxFit.fitWidth,
-                        alignment: Alignment.center,
-                      ),
-                    ),
-                    child: ListTile(
-                      contentPadding: EdgeInsets.symmetric(
-                          horizontal: 20.0, vertical: 10.0),
-                      title: Text(
-                        element.name,
-                        style: TextStyle(
-                            fontWeight: FontWeight.bold, color: Colors.white),
-                        textAlign: TextAlign.left,
-                        maxLines: 3,
-                      ),
-                      trailing: IconButton(
-                        icon: Icon(Icons.remove_red_eye, color: Colors.white),
-                        onPressed: () {
-                          state.activeRecipe = element;
-                          Navigator.pushNamed(context, '/recipe');
-                        },
-                      ),
-                    ),
-                  ),
+                  fontSize: 22,
+                  color:
+                      Provider.of<StateTracker>(context, listen: false).darkMode
+                          ? Colors.white
+                          : Colors.black,
                 ),
-              );
-            },
-          );
-        }),
-      ),
-      floatingActionButton: FloatingActionButton.extended(
-          onPressed: () {
-            Provider.of<StateTracker>(context, listen: false)
-                .fetchMoreRecipes();
-          },
-          backgroundColor: Colors.green.shade300,
-          icon: Icon(
-            Icons.add,
-            color: Colors.black,
+              ),
+              SizedBox(height: 20.0),
+              Text(
+                "Press \"Find Recipes\" to add some!",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 22,
+                  color:
+                      Provider.of<StateTracker>(context, listen: false).darkMode
+                          ? Colors.white
+                          : Colors.black,
+                ),
+              ),
+            ],
           ),
-          label: Text(
-            "Find Recipes",
-            style: TextStyle(
+        ),
+        floatingActionButton: FloatingActionButton.extended(
+            onPressed: () {
+              Provider.of<StateTracker>(context, listen: false)
+                  .fetchMoreRecipes();
+            },
+            backgroundColor: Colors.green.shade300,
+            icon: Icon(
+              Icons.add,
               color: Colors.black,
             ),
-          )),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
-    );
+            label: Text(
+              "Find Recipes",
+              style: TextStyle(
+                color: Colors.black,
+              ),
+            )),
+        floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      );
+    } else {
+      return Scaffold(
+        backgroundColor:
+            Provider.of<StateTracker>(context, listen: true).darkMode
+                ? Colors.black87
+                : Colors.white,
+        body: Padding(
+          padding: const EdgeInsets.fromLTRB(10.0, 20.0, 10.0, 75.0),
+          child: Consumer<StateTracker>(builder: (context, state, child) {
+            return GroupedListView<dynamic, String>(
+              elements: state.recipes,
+              groupBy: (element) => DateFormat.yMMMd().format(element.date),
+              groupComparator: (value1, value2) => value2.compareTo(value1),
+              itemComparator: (item1, item2) =>
+                  item1.name.compareTo(item2.name),
+              order: GroupedListOrder.DESC,
+              // useStickyGroupSeparators: true,
+              groupSeparatorBuilder: (String value) => Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text(
+                  value,
+                  textAlign: TextAlign.left,
+                  style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                      color: state.darkMode ? Colors.white : Colors.black),
+                ),
+              ),
+              itemBuilder: (c, element) {
+                return Slidable(
+                  actionPane: SlidableDrawerActionPane(),
+                  actions: <Widget>[
+                    IconSlideAction(
+                      caption: "Favourite",
+                      color: Colors.yellow[600],
+                      icon: Icons.star_border_sharp,
+                      onTap: () {},
+                    ),
+                  ],
+                  secondaryActions: <Widget>[
+                    IconSlideAction(
+                      caption: "Delete",
+                      color: Colors.red,
+                      icon: Icons.delete,
+                      onTap: () {},
+                    ),
+                  ],
+                  child: Card(
+                    elevation: 8.0,
+                    margin: new EdgeInsets.symmetric(
+                        horizontal: 10.0, vertical: 4.0),
+                    child: Container(
+                      decoration: BoxDecoration(
+                        image: DecorationImage(
+                          colorFilter: new ColorFilter.mode(
+                              Colors.black.withOpacity(0.5), BlendMode.darken),
+                          image: NetworkImage(element.imageLink),
+                          fit: BoxFit.fitWidth,
+                          alignment: Alignment.center,
+                        ),
+                      ),
+                      child: ListTile(
+                        contentPadding: EdgeInsets.symmetric(
+                            horizontal: 20.0, vertical: 10.0),
+                        title: Text(
+                          element.name,
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold, color: Colors.white),
+                          textAlign: TextAlign.left,
+                          maxLines: 3,
+                        ),
+                        trailing: IconButton(
+                          icon: Icon(Icons.remove_red_eye, color: Colors.white),
+                          onPressed: () {
+                            state.activeRecipe = element;
+                            Navigator.pushNamed(context, '/recipe');
+                          },
+                        ),
+                      ),
+                    ),
+                  ),
+                );
+              },
+            );
+          }),
+        ),
+        floatingActionButton: FloatingActionButton.extended(
+            onPressed: () {
+              Provider.of<StateTracker>(context, listen: false)
+                  .fetchMoreRecipes();
+            },
+            backgroundColor: Colors.green.shade300,
+            icon: Icon(
+              Icons.add,
+              color: Colors.black,
+            ),
+            label: Text(
+              "Find Recipes",
+              style: TextStyle(
+                color: Colors.black,
+              ),
+            )),
+        floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      );
+    }
   }
 }
